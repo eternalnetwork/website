87b0b4f6b06d26a4cba8a546b50af02280532695
frodggy <frodggyxd@gmail.com>
2023-05-05 05:28:53 +0000

    init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Hero from '~/components/Hero'
+import styles from '~/lib/styles'
+
+export default function Home() {
+  return (
+    <main className="">
+      <Hero />
+    </main>
+  )
+}
---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+import styles from '~/lib/styles'
+
+
+export default function Hero() {
+    return (
+        <section id="home" className='w-screen h-screen bg-black'>
+
+        </section>
+    )
+}
